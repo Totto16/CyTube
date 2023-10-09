e2d1513f7ff8cc3909e4b6105fe5f7f01f3db761
GenJoeyy <98785700+GenJoeyy@users.noreply.github.com>
2023-10-09 05:56:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ function setIcon() {
     div.innerHTML = '<link rel="icon" type="image/x-icon" href="https://yt3.googleusercontent.com/ytc/APkrFKaZ1POoLFYwBP_7riSuRQgp53v1j-Biw68QR4ay=s900-c-k-c0x00ffffff-no-rj">';
     document.head.appendChild(div.firstChild);
 }
+
+function setTitle() {
+    document.title = "stegiTube";    
+}
+
+function setBranding() {
+    var div = document.createElement('div');
+    div.innerHTML = '<a class="navbar-brand" href="https://twitch.tv/stegi">stegiTube</a>';
+    document.getElementsByClassName('navbar-brand')[0].replaceWith(div.firstChild);   
+}
  
-setIcon()
+setIcon();
+setTitle();
+setBranding();
