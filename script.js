bca8c4a1a56b72936357e9e902c4d08dfb048b69
GenJoeyy <98785700+GenJoeyy@users.noreply.github.com>
2023-10-09 05:41:40 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function setIcon() {
+    var div = document.createElement('div');
+    div.innerHTML = '<link rel="icon" type="image/x-icon" href="https://yt3.googleusercontent.com/ytc/APkrFKaZ1POoLFYwBP_7riSuRQgp53v1j-Biw68QR4ay=s900-c-k-c0x00ffffff-no-rj">';
+    document.head.appendChild(div.firstChild);
+}
+ 
+setIcon()
